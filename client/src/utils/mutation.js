248cc8fdcd52d7104105888b99125b25e2745d37
akaur091 <amniderkaur.09@gmail.com>
2022-06-03 22:07:07 -0700

    add code to execute mutation for addUser

--- client/src/utils/mutation.js
+++ client/src/utils/mutation.js
@@ -6,15 +6,25 @@ export const LOGIN_USER = gql`
     mutation login($email: String!, $password:Sting!){
         login(email: $email, password: $password) {
             token
-            profile {
+            user {
                 _id
-                name
+                username
             }
         }
     }
 `;
 // TODO: ADD_USER - execute addUser
-
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email:String!, $password: String!){
+        addUser(username: $username, email: $email, password: $password){
+            token
+            user{
+                _id
+                username
+            }
+        }
+    }
+`;
 // TODO: SAVE_BOOK - exectue saveBook
 
 // TODO: REMOVE_BOOK - execute removeBook
