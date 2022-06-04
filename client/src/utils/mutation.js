29fea0ba14a6b5a7d226ad321f1d51b3f546ed34
akaur091 <amniderkaur.09@gmail.com>
2022-06-03 22:02:32 -0700

    add LOGIN_USER in mutation

--- client/src/utils/mutation.js
+++ client/src/utils/mutation.js
@@ -1,4 +1,20 @@
+// import gql
+import{ gql } from '@apollo/client';
+
 // TODO: LOGIN_USER - execute loginUser
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password:Sting!){
+        login(email: $email, password: $password) {
+            token
+            profile {
+                _id
+                name
+            }
+        }
+    }
+`;
 // TODO: ADD_USER - execute addUser
+
 // TODO: SAVE_BOOK - exectue saveBook
+
 // TODO: REMOVE_BOOK - execute removeBook
