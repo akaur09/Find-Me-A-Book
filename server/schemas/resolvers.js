5b28a40abec967aa5922ada93b11331373e49575
akaur091 <amniderkaur.09@gmail.com>
2022-06-03 21:11:33 -0700

    add mutation code for adding a user and login

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -16,6 +16,25 @@ const resolvers = {
         },
     },
     Mutation: {
+        addUser: async (parent, {username, email, password}) => {
+            const user = await User.create({
+                username, email, password
+            });
+            const token = signToken(user);
+            return {token, user};
+        },
+        login: async (parent, { email, password}) => {
+            const user = await User.findOne({emial});
+            if(!user){
+                throw new AuthenticationError ('No user with this email found');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw){
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const token = signToken(user);
+            return {token, user};
+        },
         
     }
 }
