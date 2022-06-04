6e74dbc920bb82898b5ef374b7c360a725ecadce
akaur091 <amniderkaur.09@gmail.com>
2022-06-03 21:59:00 -0700

    fix import tag

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,19 @@
 // TODO: add the query of GET_ME to execute the me quesry using Apollo Server
+import{ gql } from '@apollo/client';
+
+export const GET_ME = gql `{
+    me{
+        _id
+        username
+        email
+        bookCount
+        savedBooks{
+            bookId
+            authors
+            image
+            link
+            title
+            description
+        }
+    }
+}`;
